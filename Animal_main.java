12eaf019a2544da745c600e45249f6551b267d9e
sumaiya69 <31273072+sumaiya69@users.noreply.github.com>
2017-10-19 15:45:31 +0600

    all file is commited

---
+++ Animal_main.java
@@ -0,0 +1,10 @@
+package lab_project;
+
+public class Animal_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Insectes.java
@@ -0,0 +1,5 @@
+package lab_project;
+
+public class Insectes {
+
+}
